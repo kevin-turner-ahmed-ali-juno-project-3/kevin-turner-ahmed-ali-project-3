b404db50b72c12ffe0c7e3a4220efa9227d83c94
Ahmed <ahmedali.96@hotmail.com>
2021-04-09 16:15:22 -0400

    created create blog component

--- simple-blog/src/App.js
+++ simple-blog/src/App.js
@@ -1,12 +1,14 @@
-import DisplayBlog from './DisplayBlog';
-import CreateBlog from './CreateBlog';
-import firebase from './firebase';
+import DisplayBlog from "./DisplayBlog";
+import CreateBlog from "./CreateBlog";
+import firebase from "./firebase";
 
 function App() {
   return (
     <div className="App">
       <h1>Simple Blog</h1>
       <DisplayBlog />
+
+      <CreateBlog />
     </div>
   );
 }
--- simple-blog/src/CreateBlog.js
+++ simple-blog/src/CreateBlog.js
@@ -1,17 +1,45 @@
-import firebase from 'firebase';
+import firebase from "firebase";
+import { useState } from "react";
 
-function CreateBlog() { 
-    return (
-        <div>
-            
-            {/* 
+function CreateBlog() {
+  const [name, setName] = useState("");
+  const [content, setContent] = useState("");
+
+  const handleClick = (e) => {
+    e.preventDefault();
+
+    const dbRef = firebase.database().ref();
+    dbRef.push({ person: name, writing: content });
+  };
+
+  return (
+    <div>
+      {/* 
                 person creates a blog, attaches their name, that blog is sent to firebase
                 create object containing person name and content of blog post as params
             
             */}
-        
-        </div>
-    )
+
+      <form action="submit">
+        <label htmlFor="nameInput">Name</label>
+        <input
+          type="text"
+          id="nameInput"
+          onChange={(e) => setName(e.target.value)}
+        ></input>
+
+        <label htmlFor="contentInput">Content</label>
+        <textarea
+          id="contentInput"
+          onChange={(e) => setContent(e.target.value)}
+        ></textarea>
+
+        <button id="submit" onClick={handleClick}>
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default CreateBlog;
