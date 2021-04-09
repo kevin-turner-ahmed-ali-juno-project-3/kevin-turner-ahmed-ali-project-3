36b9b3c17a96460f0ee10ff8e6ab49539d1f4a5a
TenEighths <kevin.turner84@gmail.com>
2021-04-09 12:50:19 -0700

    proof of concept print to page from DB

--- simple-blog/src/CreateBlog.js
+++ simple-blog/src/CreateBlog.js
@@ -3,7 +3,13 @@ import firebase from 'firebase';
 function CreateBlog() { 
     return (
         <div>
-
+            
+            {/* 
+                person creates a blog, attaches their name, that blog is sent to firebase
+                create object containing person name and content of blog post as params
+            
+            */}
+        
         </div>
     )
 }
